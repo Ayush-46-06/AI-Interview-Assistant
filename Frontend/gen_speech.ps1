cfeddec245f081bd1961e0814a200f3705597325
Mukund Jha <mukundjha728@gmail.com>
2026-09-11 00:54:32 +0530

    chore: remove test artifacts

--- Frontend/gen_speech.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Speech
-$synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$synth.SetOutputToWaveFile("test_speech.wav")
-$synth.Speak("Hello, this is a test of the speech recognition system for my interview.")
-$synth.Dispose()
